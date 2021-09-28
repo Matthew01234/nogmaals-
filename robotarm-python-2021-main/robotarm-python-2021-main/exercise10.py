301b47d482fd3895387f48382a70d3333fa45950
Matthew <99067338@mydavinci.nl>
2021-09-28 10:53:10 +0200

    level 10

--- robotarm-python-2021-main/robotarm-python-2021-main/exercise10.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/exercise10.py
@@ -1,10 +1,15 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 10')
+for i in range (0,5):
+    robotArm.grab()
+    for z in range(0,9-i * 2):
+     robotArm.moveRight()
+    
+    robotArm.drop()
+    for y in range(0,8-i * 2):
+     robotArm.moveLeft()
+  
 
-# Jouw python instructies zet je vanaf hier:
 
-
-
-# Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
