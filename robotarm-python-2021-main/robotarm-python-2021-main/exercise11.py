95cba5e43b82cb638c0e402a421b50f2fda3d10a
Matthew <99067338@mydavinci.nl>
2021-09-28 11:18:36 +0200

    level 11

--- robotarm-python-2021-main/robotarm-python-2021-main/exercise11.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/exercise11.py
@@ -3,6 +3,16 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 11')
 
 # Jouw python instructies zet je vanaf hier:
+color = robotArm.scan()
+
+for i in range (0,11):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == ('white'):
+     robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveRight()
+
 
 
 
