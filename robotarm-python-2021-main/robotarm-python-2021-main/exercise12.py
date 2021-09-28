3ef1c3f7e5c27bce0420325034281d46cb6e14d5
Matthew <99067338@mydavinci.nl>
2021-09-28 13:22:17 +0200

    level 12

--- robotarm-python-2021-main/robotarm-python-2021-main/exercise12.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/exercise12.py
@@ -4,7 +4,24 @@ robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
 
+robotArm.speed = 2
+for i in range (20):
+    robotArm.grab()
+    robotArm.scan()
+    color = robotArm.scan()
+    if color == 'red':
+        for i in range (9):
+            robotArm.moveRight()
+            
+        robotArm.drop()
+        for i in range (9):
+            robotArm.moveLeft()
 
+    else:
+         robotArm.drop()
+         robotArm.moveRight()
 
+  
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
+
